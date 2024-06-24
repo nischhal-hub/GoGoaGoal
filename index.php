d27243ae05538290c3a00d8a3b735ea2392d96db
nischhal-nub <nischalbh51@gmail.com>
2024-06-24 16:34:04 +0545

    ui: primary and secondary buttons

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ include './includes/toppart.php';
     <h2>Gogoagoal</h2>
     <h3>Gogoagoal</h3>
     <p>Gogoagoal</p>
+    <button class="primary-btn">Primary</button>
+    <button class="secondary-btn">Secondary</button>
 </body>
 
 </html>
