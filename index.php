92cb5c7ae6e6a5bf9231fb87e4c267dcdf993baa
nischhal-nub <nischalbh51@gmail.com>
2024-06-24 16:03:18 +0545

    add: normalise css

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ include './includes/toppart.php';
 
 <body>
     <img src="assets/Gogoagoal.png" alt="logo">
-    
+    <h1>Gogoagoal</h1>
+    <h2>Gogoagoal</h2>
+    <h3>Gogoagoal</h3>
+    <p>Gogoagoal</p>
 </body>
 
 </html>
