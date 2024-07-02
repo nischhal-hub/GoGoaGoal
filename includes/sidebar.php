a1dddbcb2e12049f495d1708f020d60b14a5084e
nischhal-nub <nischalbh51@gmail.com>
2024-07-02 17:15:49 +0545

    feat:add reports page

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,15 +3,15 @@
             <h2>GoGoaGoal</h2>
             <ul>
                 <li><img src="./assets/icons/icons8-home-60.png" height="25" width="25" alt="home icon">&nbsp;&nbsp;<a
-                        href="/index.php">Dashboard</a></li>
+                        href="./index.php">Dashboard</a></li>
                 <li><img src="./assets/icons/icons8-bicycle-kick-51.png" height="25" width="25" alt="form icon">&nbsp;&nbsp;<a
                         href="/issueform.html">Book Futsal</a></li>
                 <li><img src="./assets/icons/icons8-trophy-60.png" height="25" width="25" alt="book icon">&nbsp;&nbsp;<a
                         href="/itemrecords.html">Tournaments</a></li>
                 <li><img src="./assets/icons/icons8-staff-48.png" height="25" width="25" alt="book icon">&nbsp;&nbsp;<a
                         href="/returnform.html">Staff</a></li>
-                <li><img src="./assets/icons/icons8-return-32.png" height="25" width="25" alt="book icon">&nbsp;&nbsp;<a
-                        href="/returnrecords.html">Disabled</a></li>
+                <li><img src="./assets/icons/icons8-reports-48.png" height="25" width="25" alt="book icon">&nbsp;&nbsp;<a
+                        href="./reports.php">Reports</a></li>
             </ul>
         </div>
 </aside>
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -33,64 +33,9 @@
             </tr>
         </table>
     </div>
-</section>
-<section class="reports">
-    <div class="slots-graph">
-        <div>
-            <h3>Booking reports</h3>
-            <div class="booking-grid-table">
-                <table>
-                    <tr>
-                        <th>Date</th>
-                        <th>No of bookings</th>
-                        <th>Status</th>
-                    </tr>
-                    <tr>
-                        <td>2024/07/2</td>
-                        <td>10</td>
-                        <td><img src="assets/icons/icons8-up-52.png" alt="arrow" height="18" width="18"></td>
-                    </tr>
-                    <tr>
-                        <td>2024/07/2</td>
-                        <td>10</td>
-                        <td><img src="assets/icons/icons8-down.png" alt="arrow" height="18" width="18"></td>
-                    </tr>
-                    <tr>
-                        <td>2024/07/2</td>
-                        <td>10</td>
-                        <td><img src="assets/icons/icons8-up-52.png" alt="arrow" height="18" width="18"></td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div class="income-graph">
-        <div>
-            <h3>Income reports</h3>
-            <div class="income-grid-table">
-                <table>
-                    <tr>
-                        <th>Date</th>
-                        <th>Income(in Rs)</th>
-                        <th>Status</th>
-                    </tr>
-                    <tr>
-                        <td>2024/07/2</td>
-                        <td>1000</td>
-                        <td><img src="assets/icons/icons8-up-52.png" alt="arrow" height="18" width="18"></td>
-                    </tr>
-                    <tr>
-                        <td>2024/07/2</td>
-                        <td>5000</td>
-                        <td><img src="assets/icons/icons8-down.png" alt="arrow" height="18" width="18"></td>
-                    </tr>
-                    <tr>
-                        <td>2024/07/2</td>
-                        <td>6000</td>
-                        <td><img src="assets/icons/icons8-up-52.png" alt="arrow" height="18" width="18"></td>
-                    </tr>
-                </table>
-            </div>
+    <div class="summary-container">
+        <div class="box1">
+
         </div>
     </div>
 </section>
