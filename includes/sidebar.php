ef6a1cf2224704645dc18c733a5a83e74d72fecd
nischhal-nub <nischalbh51@gmail.com>
2024-07-19 22:25:21 +0545

    wip: updated sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,17 +1,21 @@
 <aside class="sidebar" id="sidebar">
         <div class="sidebar-container">
-            <h2>GoGoaGoal</h2>
-            <ul>
-                <li><span class="sidebar-icon"><i class="fa-solid fa-house fa-2xl"></i></span>&nbsp;&nbsp;<a
-                        href="./index.php">Dashboard</a></li>
-                <li><span class="sidebar-icon"><i class="fa-solid fa-futbol fa-2xl"></i></span>&nbsp;&nbsp;<a
-                        href="./futsalbookform.php">Book Futsal</a></li>
-                <li><span class="sidebar-icon"><i class="fa-solid fa-trophy fa-2xl"></i></span>&nbsp;&nbsp;<a
-                        href="/itemrecords.html">Tournaments</a></li>
-                <li><span class="sidebar-icon"><i class="fa-solid fa-users fa-2xl"></i></span>&nbsp;&nbsp;<a
-                        href="./staff.php">Staff</a></li>
-                <li><span class="sidebar-icon"><i class="fa-solid fa-square-poll-vertical fa-2xl"></i></span>&nbsp;&nbsp;<a
-                        href="./reports.php">Reports</a></li>
-            </ul>
+                <h2>GoGoaGoal</h2>
+                <ul>
+                        <li><span class="sidebar-icon"><i class="fa-solid fa-house fa-2xl"></i></span>&nbsp;&nbsp;<a
+                                        href="./index.php">Dashboard</a></li>
+                        <li><span class="sidebar-icon"><i class="fa-solid fa-futbol fa-2xl"></i></span>&nbsp;&nbsp;<a
+                                        href="./futsalbookform.php">Book Futsal</a></li>
+                        <li><span class="sidebar-icon"><i
+                                                class="fa-solid fa-file-invoice-dollar fa-2xl"></i></span>&nbsp;&nbsp;<a
+                                        href="/itemrecords.html">Track Expenditure</a></li>
+                        <li><span class="sidebar-icon"><i class="fa-solid fa-money-bills fa-2xl"></i></span>&nbsp;&nbsp;<a
+                                        href="/itemrecords.html">Checkout</a></li>
+                        <li><span class="sidebar-icon"><i class="fa-solid fa-users fa-2xl"></i></span>&nbsp;&nbsp;<a
+                                        href="./staff.php">Staff</a></li>
+                        <li><span class="sidebar-icon"><i
+                                                class="fa-solid fa-square-poll-vertical fa-2xl"></i></span>&nbsp;&nbsp;<a
+                                        href="./reports.php">Reports</a></li>
+                </ul>
         </div>
 </aside>
