67aa8b820b562879b892afb53dedac9bcc8e060b
nischhal-nub <nischalbh51@gmail.com>
2024-07-28 10:58:59 +0545

    feat: staff images upload successfull

--- pages/staff-list.php
+++ pages/staff-list.php
@@ -2,9 +2,12 @@
             <h3>Staff details</h3>
             <div class="filter-container">
                 <form class="filter-form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="get" style="width:100%; border-radius:none; box-shadow:none; padding:0;margin:0; gap:2rem;">
-                    <input type="date" name="filterDate" style="width: 200px;" required>
-                    <button class="primary-btn" style="margin-left:2rem;"><i class="fa-solid fa-filter"></i>&nbsp;Filter</button>
+                    <input type="text" name="searchParam" placeholder="Search by name..." style="width: 200px;" required>
+                    <button class="secondary-btn" style="margin-left:2rem;"><i class="fa-solid fa-search"></i>&nbsp;Search</button>
                 </form>
+               
+                    <button class="primary-btn"><a href="./add-staff.php"  style="display:flex;"><i class="fa-solid fa-plus"></i>&nbsp;Staff</a></button>
+               
             </div>
             <table class="booking-details-table">
                 <tr>
@@ -24,7 +27,7 @@
                     <?php foreach ($staffsArr as $list): ?>
                         <tr>
                             <td><?= htmlspecialchars($i) ?></td>
-                            <td><img src="image.php?id=<?php echo htmlspecialchars($list['staff_id']); ?>" alt="avatar" width="50px" height="50px"/><?= htmlspecialchars($list['staff_name']) ?></td>
+                            <td style="display:flex; align-items:center; justify-content:center;""><?php echo '<img class="avatar-img" src="data:image/jpeg;base64,'.base64_encode($list['staff_avatar']).'"/>'?> <?= htmlspecialchars($list['staff_name'])?> </td>
                             <td><?= htmlspecialchars($list['staff_contact']) ?></td>
                             <td><?= htmlspecialchars($list['staff_email']) ?></td>
                             <td><?= htmlspecialchars($list['staff_join_date']) ?></td>
