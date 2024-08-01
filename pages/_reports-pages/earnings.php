39bbc5d11798fee620408b7a2f6e7246b14267bb
nischhal-nub <nischalbh51@gmail.com>
2024-08-01 09:48:36 +0545

    feat: income report component

--- pages/_reports-pages/earnings.php
+++ pages/_reports-pages/earnings.php
@@ -1,5 +1,20 @@
+<?php
+$sql = "SELECT * FROM checkout";
+$result = $conn->query($sql);
+$incomeArr = [];
+$total = 0;
+$prevAmount = 0;
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $incomeArr[] = $row;
+        $total += $row['amount']
+;
+    }
+}
+?>
 <div>
-<div class="income-graph">
+    <div class="income-graph">
         <div>
             <h3>Income reports</h3>
             <div class="income-grid-table">
@@ -9,20 +24,36 @@
                         <th>Income(in Rs)</th>
                         <th>Status</th>
                     </tr>
+                    <?php foreach ($incomeArr as $income): ?>
+                        <tr>
+                            <td><?= htmlspecialchars($income['checkout_at']) ?></td>
+                            <td>Rs.<?= htmlspecialchars($income['amount']) ?></td>
+                            <td><?php
+                            if($prevAmount == 0) {
+                                $percentage = 100;
+                            }
+                            else{
+                                $percentage =number_format(abs(($income['amount'] - $prevAmount)) / $prevAmount * 100,1) ;
+                            }
+                            if ($income['amount'] > $prevAmount) { ?>
+                                <div class="up">
+                                    <div><img src="./assets/icons/icons8-up-52.png" alt="arrow" height="18" width="18"></div>
+                                    <p><?php echo $percentage ?> % increased than yesterday</p>
+                                </div>
+                            <?php } else { ?>
+                                <div class="up">
+                                <img src="./assets/icons/icons8-down.png" alt="arrow" height="18" width="18">
+                                <span><?php echo $percentage?> % decreased than yesterday</span>
+                                </div>
+                            <?php } ?>
+                            </td>
+                            <?php $prevAmount = $income['amount']; ?>
+                        </tr>
+                    <?php endforeach; ?>
                     <tr>
-                        <td>2024/07/2</td>
-                        <td>1000</td>
-                        <td><img src="./assets/icons/icons8-up-52.png" alt="arrow" height="18" width="18"></td>
-                    </tr>
-                    <tr>
-                        <td>2024/07/2</td>
-                        <td>5000</td>
-                        <td><img src="./assets/icons/icons8-down.png" alt="arrow" height="18" width="18"></td>
-                    </tr>
-                    <tr>
-                        <td>2024/07/2</td>
-                        <td>6000</td>
-                        <td><img src="./assets/icons/icons8-up-52.png" alt="arrow" height="18" width="18"></td>
+                        <td style="font-weight: 700;">Total:</td>
+                        <td style="font-weight: 700;">Rs.<?= $total ?></td>
+                        <td></td>
                     </tr>
                 </table>
             </div>
--- reports.php
+++ reports.php
@@ -34,7 +34,7 @@ if ($result->num_rows > 0) {
         <div class="summary">
             <h3>Lifetime Reports</h3>
             <div class="summary-container">
-                <div class="box" style="margin-left: 0;">
+                <div class="box report-box" style="margin-left: 0;">
                     <div class="result">
                         <p><?php echo $resultArray["bookingSummarySql"] ?></p>
                         <p>Total Bookings</p>
@@ -43,7 +43,7 @@ if ($result->num_rows > 0) {
                         <i class="fa-brands fa-font-awesome fa-2xl" style="color: var(--primary)"></i>
                     </div>
                 </div>
-                <div class="box">
+                <div class="box report-box">
                     <div class="result">
                         <p><?php echo $resultArray["earningSummarySql"] == null ? '0' : $resultArray["earningSummarySql"] ?>
                         </p>
@@ -53,7 +53,7 @@ if ($result->num_rows > 0) {
                         <i class="fa-solid fa-money-bills fa-2xl" style="color: var(--primary)"></i>
                     </div>
                 </div>
-                <div class="box">
+                <div class="box report-box">
                     <div class="result">
                         <p><?php echo $resultArray["expenditureSummarySql"] == null ? '0' : $resultArray["expenditureSummarySql"] ?>
                         </p>
@@ -63,7 +63,7 @@ if ($result->num_rows > 0) {
                         <i class="fa-solid fa-file-invoice-dollar fa-2xl" style="color: var(--primary)"></i>
                     </div>
                 </div>
-                <div class="box" style="margin-right: 0;">
+                <div class="box report-box" style="margin-right: 0;">
                     <div class="result">
                         <p><?php echo $resultArray["staffsSql"] ?></p>
                         <p>Total Staffs</p>
